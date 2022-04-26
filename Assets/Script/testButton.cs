0f5907a47c99bedea7a027ef5614a5aaa800dc71
KadogameBaruto <cardgamebattle@gmail.com>
2022-04-26 23:22:57 +0900

    はつこみ

---
+++ Assets/Script/testButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testButton : MonoBehaviour
+{
+    public Text a;
+
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.text = count++.ToString();
+    }
+
+    public void OnClick()
+    {
+        count = 0;
+
+    }
+
+
+}
